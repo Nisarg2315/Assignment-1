d635caf710123c3b1a177a6f451fd244a281271b
Nisarg2315 <nisarg2315@gmail.com>
2025-08-17 23:46:48 +0530

    Add files via upload

---
+++ cal. square.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float L;
+    printf("Enter side length: ");
+    scanf("%f", &L);
+    printf("Area = %.2f\n", L * L);
+    printf("Perimeter = %.2f\n", 4 * L);
+    return 0;
+}
