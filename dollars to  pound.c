690ae2ae9ba4555cd10d7bb998c3731150752db7
Nisarg2315 <nisarg2315@gmail.com>
2025-08-17 23:36:03 +0530

    Add files via upload

---
+++ dollars to  pound.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float dollars, rs, pounds;
+    printf("Enter dollars: ");
+    scanf("%f", &dollars);
+    rs = dollars * 48;
+    pounds = rs / 70;
+    printf("%.2f dollars = %.2f pounds\n", dollars, pounds);
+    return 0;
+}
